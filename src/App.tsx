f0251039f7ba6d6b1933e441d3ea6809785b4e29
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2023-08-28 23:58:15 -0300

    banner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Home } from './pages/home/Home.tsx'
-import { Menu } from './components/Menu/Menu.tsx'
 import './app.scss'
 
 function App() {
     return (
         <>
             <Home />
-            <Menu />
         </>
     )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,22 @@
 import { Header } from '../../components/Header/Header.tsx';
+import { Menu } from '../../components/Menu/Menu.tsx';
+import banner from './banner.png'
+import './Home.styles.scss'
 
 export function Home() {
     return (
-        <Header />
+        <>
+            <Header />
+            <main>
+                <section className='principal'>
+                    <Menu />
+                    <div className='imagem'>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt='Terra vista do espaço' />
+                    </div>
+                </section>
+            </main>
+        </>
+
     )
 }
